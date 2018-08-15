620c2edf9c59ce22c4031e78935eaa73bc424db4
Anton Yatskevich <Anton_Yatskevich@epam.com>
2018-08-15 11:57:02 +0300

    style: eslint init

--- cardsCreator.js
+++ cardsCreator.js
@@ -1,60 +1,57 @@
 const cardImgs = [
-    'http://pluspng.com/img-png/credit-card-png-credit-card-png-image-708.png',
-    'https://www.imbank.com/rwanda/wp-content/uploads/2014/08/gold-credit-card-Recovered.png',
-    'http://info.westpac.com.au/creditcards/assets/images/55day_Classic_VISA_PayWave_exp-0000.png',
-    'https://www.cashoverflow.in/wp-content/uploads/2016/05/Standard-Chartered-Platinum-Credit-Card.png',
-    'https://cdn.thesimpledollar.com/wp-content/uploads/2018/01/First-Progress-Platinum-Prestige-MasterCard%C2%AE-Secured-Credit-Card.png',    
-    'https://www.telegraph.co.uk/content/dam/financial-services/Money%20Comparison/Credit%20Cards/assets/red-credit-card-400_tcm97-55103.png?imwidth=450',
-    'https://www.regionalaustraliabank.com.au/personal/products/url/-/media/CommunityMutual/Images/Content/Products/Cards/Your_Choice_Visa.png',
-    'https://www.smile.co.uk/assets/ns/smile/img/pattern-library/img-gold-credit-card.png'
+	'http://pluspng.com/img-png/credit-card-png-credit-card-png-image-708.png',
+	'https://www.imbank.com/rwanda/wp-content/uploads/2014/08/gold-credit-card-Recovered.png',
+	'http://info.westpac.com.au/creditcards/assets/images/55day_Classic_VISA_PayWave_exp-0000.png',
+	'https://www.cashoverflow.in/wp-content/uploads/2016/05/Standard-Chartered-Platinum-Credit-Card.png',
+	'https://cdn.thesimpledollar.com/wp-content/uploads/2018/01/First-Progress-Platinum-Prestige-MasterCard%C2%AE-Secured-Credit-Card.png',
+	'https://www.telegraph.co.uk/content/dam/financial-services/Money%20Comparison/Credit%20Cards/assets/red-credit-card-400_tcm97-55103.png?imwidth=450',
+	'https://www.regionalaustraliabank.com.au/personal/products/url/-/media/CommunityMutual/Images/Content/Products/Cards/Your_Choice_Visa.png',
+	'https://www.smile.co.uk/assets/ns/smile/img/pattern-library/img-gold-credit-card.png',
 ];
 
 const cardTypes = [
-    'Balance transfer',
-    'Purchase',
-    'Spend abroad',
+	'Balance transfer',
+	'Purchase',
+	'Spend abroad',
 ];
 
 const createCards = (images, types, amount) => {
-    const cards = [];
-    let i = 1;
+	const cards = [];
 
-    for (i; i <= amount; i++) {
-        let imageId = Math.floor(Math.random() * images.length);
-        let typeId =  Math.floor(Math.random() * types.length);
-        let balanceFeeMonths = Math.floor(Math.random() * 12 + 1);
-        let balanceFeeAmount = Math.floor(Math.random() * 12 + balanceFeeMonths);
-        let costAmount = Math.floor(Math.random() * 100 + 10) * 10;
-        let costMonths = Math.floor(Math.random() * 12 + 1);
-        let exmpSpend = Math.floor(Math.random() * 100 + 10) * 10;
-        let exmpPurchase = Math.floor(Math.random() * 10 + 10) * 10;
+	for (let i = 1; i <= amount; i + 1) {
+		const imageId = Math.floor(Math.random() * images.length);
+		const typeId = Math.floor(Math.random() * types.length);
+		const balanceFeeMonths = Math.floor(Math.random() * 12 + 1);
+		const balanceFeeAmount = Math.floor(Math.random() * 12 + balanceFeeMonths);
+		const costAmount = Math.floor(Math.random() * 100 + 10) * 10;
+		const costMonths = Math.floor(Math.random() * 12 + 1);
+		const exmpSpend = Math.floor(Math.random() * 100 + 10) * 10;
+		const exmpPurchase = Math.floor(Math.random() * 10 + 10) * 10;
 
-        let card = {
-            id: `CARD_${i}`,
-            name: `BANK_${i}`,
-            image: images[imageId],
-            type: cardTypes[typeId],
-            repayments: {
-                balanceFee: {
-                    amount: balanceFeeAmount,
-                    months: balanceFeeMonths
-                },
-                costToYou: {
-                    amount: costAmount,
-                    months: costMonths
-                }
-            },
-            representativeExmp: {
-                spend: exmpSpend,
-                purchase: exmpPurchase
-            }
-        }
-        cards.push(card);
-    } 
-       
-    return cards;
-} 
-
+		const card = {
+			id: `CARD_${i}`,
+			name: `BANK_${i}`,
+			image: images[imageId],
+			type: cardTypes[typeId],
+			repayments: {
+				balanceFee: {
+					amount: balanceFeeAmount,
+					months: balanceFeeMonths,
+				},
+				costToYou: {
+					amount: costAmount,
+					months: costMonths,
+				},
+			},
+			representativeExmp: {
+				spend: exmpSpend,
+				purchase: exmpPurchase,
+			},
+		};
+		cards.push(card);
+	}
+	return cards;
+};
 
 const cards = createCards(cardImgs, cardTypes, 30);
 
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+
 const app = express();
 const data = require('./cardsCreator');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}); 
-
-app.get('/', function (req, res) {
-    res.send(data);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.listen(3000, function () {
-    console.log('Server started');
+app.get('/', (req, res) => {
+	res.send(data);
+});
+
+app.listen(3000, () => {
+	console.log('Server started');
 });
